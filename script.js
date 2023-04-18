7535a3569f50928fc7c21160f9bc7f1d4db2a214
Alyona Semyonova <invisible2234@gmail.com>
2023-04-18 23:17:33 +0700

    Урок 4. Регулярные выражения

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const API_URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/";
 /**
  * Класс карточки товара
@@ -167,3 +169,86 @@ class GoodsList extends List {
 const list = new GoodsList();
 list.fetchGoods();
 list.render();
+
+
+
+// форма обратной связи из ДЗ Урок 4 Регулярные выражения 
+const form = document.querySelector('#feedback-form');
+const nameInput = form.querySelector('#name');
+const phoneInput = form.querySelector('#phone');
+const emailInput = form.querySelector('#email');
+
+// Функция для проверки имени
+function validateName() {
+    const regex = /^[a-zA-Zа-яА-ЯёЁ]+$/;
+    if (!regex.test(nameInput.value)) {
+        nameInput.classList.add('invalid');
+        return false;
+    } else {
+        nameInput.classList.remove('invalid');
+        return true;
+    }
+}
+
+// Функция для проверки телефона
+function validatePhone() {
+    const regex = /^\+7\(\d{3}\)\d{3}-\d{4}$/;
+    if (!regex.test(phoneInput.value)) {
+        phoneInput.classList.add('invalid');
+        return false;
+    } else {
+        phoneInput.classList.remove('invalid');
+        return true;
+    }
+}
+
+// Функция для проверки e-mail
+function validateEmail() {
+    const regex = /^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,}$/;
+    if (!regex.test(emailInput.value)) {
+        emailInput.classList.add('invalid');
+        return false;
+    } else {
+        emailInput.classList.remove('invalid');
+        return true;
+    }
+}
+
+// Обработчик отправки формы
+form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Отменяем стандартное поведение формы
+
+    // Проверяем все поля на валидность
+    const isNameValid = validateName();
+    const isPhoneValid = validatePhone();
+    const isEmailValid = validateEmail();
+
+    // Если все поля валидны, отправляем форму
+    if (isNameValid && isPhoneValid && isEmailValid) {
+        const formData = new FormData(form);
+        fetch(form.action, {
+            method: form.method,
+            body: formData
+        }).then(response => {
+            alert('Форма отправлена');
+            form.reset();
+        }).catch(error => {
+            alert('Произошла ошибка при отправке формы.');
+        });
+    } else {
+        alert('Пожалуйста, заполните форму корректно.');
+    }
+});
+
+// Обработчики изменения полей для удаления класса invalid
+nameInput.addEventListener('input', () => {
+    nameInput.classList.remove('invalid');
+});
+
+phoneInput.addEventListener('input', () => {
+    phoneInput.classList.remove('invalid');
+});
+
+emailInput.addEventListener('input', () => {
+    emailInput.classList.remove('invalid');
+});
