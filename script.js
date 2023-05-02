4afde16e579f313a36bcdafa8495ab94cec504c9
Alyona Semyonova <invisible2234@gmail.com>
2023-05-02 22:35:37 +0700

    Урок 6. Компоненты Vue.js

--- script.js
+++ script.js
@@ -1,5 +1,79 @@
 const API_URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/";
 
+Vue.component('goods-list', {
+    props: ['goods'],
+    template: `<div class="goods-list">
+    <template v-if="this.goods">
+        <div class="goods-item" v-for="good in goods" :key="good.id_product">
+            <h3 @click="addToCart(good)">{{ good.product_name }}</h3>
+            <p>{{ good.price }}</p>
+        </div>
+    </template>
+    <template v-else> Список пуст</template>
+
+</div>`,
+    data() {
+        return {
+
+        }
+    }
+});
+
+Vue.component('goods-item', {
+    props: ['good'],
+    template: `
+      <div class="goods-item">
+        <h3>{{ good.product_name }}</h3>
+        <p>{{ good.price }}</p>
+      </div>
+    `,
+    data() {
+        return {
+
+        }
+    }
+});
+
+Vue.component('cart', {
+    props: ["isVisibleCart"],
+    template: `
+    <div class="modal" v-if="isVisibleCart">
+    <span class="close" @click="onToggleCart">x</span>
+</div>
+    `,
+    data() {
+        return {
+
+        }
+    },
+
+    methods: {
+        onToggleCart() {
+            this.$emit("on-toggle-cart");
+        },
+    },
+});
+
+
+Vue.component('search', {
+    template: `
+    <div>
+    <input type="text" class="goods-search" v-model="searchLine">
+    <button class="search button" type="button" @click="onSearch">Искать</button>
+    </div> `,
+    data() {
+        return {
+            searchLine: '',
+        }
+    },
+    methods: {
+        onSearch() {
+            this.$emit("on-search", this.searchLine);
+        }
+    }
+});
+
+
 const app = new Vue({
     el: '#app',
     data: {
@@ -8,6 +82,7 @@ const app = new Vue({
         cart: [],
         searchLine: '',
         isVisibleCart: false,
+
     },
 
     methods: {
@@ -30,7 +105,10 @@ const app = new Vue({
                 })
         },
         addToCart() {
-            fetch(`${API_URL}addToBasket.json`)
+            fetch(`${API_URL}addToBasket.json`, {
+                    method: 'POST',
+                    body: JSON.stringify(good)
+                })
                 .then(() => {
                     this.cart.push(good);
                 })
@@ -43,30 +121,30 @@ const app = new Vue({
                 })
         },
 
-        onSearch() {
-            const reg = new RegExp(this.searchLine, 'i')
-            this.filtered = this.goods.filter((good) => reg.test(good.product_name))
+        onSearch(searchLine) {
+            if (searchLine === '') {
+                this.filteredGoods = this.goods;
+            } else {
+                this.filteredGoods = this.goods.filter((good) => good.product_name == searchLine);
+            }
+
         },
         onToggleCart() {
             this.isVisibleCart = !this.isVisibleCart;
+        },
+        render() {
+            let listHtml = '';
+            this.goods.forEach(good => {
+                const goodItem = new GoodsItem(good.title, good.price, good.id);
+                listHtml += goodItem.render();
+            });
+            this._el.innerHTML = listHtml;
         }
     },
 
     mounted() {
-        fetch(`${API_URL}catalogData.json`)
-            .then((res) => {
-                return res.json();
-            })
-            .then((req) => {
-                this.goods = req.map(good => ({
-                    title: good.product_name,
-                    price: good.price
-                }))
-                this.render();
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+        this.loadGoods();
+        this.loadCart();
     },
 
 });
@@ -249,88 +327,3 @@ class GoodsList extends List {
 
 const cart = new Cart();
 const list = new GoodsList();
-// // list.fetchGoods();
-// // list.render();
-
-
-
-// // форма обратной связи из ДЗ Урок 4 Регулярные выражения 
-const form = document.querySelector('#feedback-form');
-const nameInput = form.querySelector('#name');
-const phoneInput = form.querySelector('#phone');
-const emailInput = form.querySelector('#email');
-
-// Функция для проверки имени
-function validateName() {
-    const regex = /^[a-zA-Zа-яА-ЯёЁ]+$/;
-    if (!regex.test(nameInput.value)) {
-        nameInput.classList.add('invalid');
-        return false;
-    } else {
-        nameInput.classList.remove('invalid');
-        return true;
-    }
-}
-
-// Функция для проверки телефона
-function validatePhone() {
-    const regex = /^\+7\(\d{3}\)\d{3}-\d{4}$/;
-    if (!regex.test(phoneInput.value)) {
-        phoneInput.classList.add('invalid');
-        return false;
-    } else {
-        phoneInput.classList.remove('invalid');
-        return true;
-    }
-}
-
-// Функция для проверки e-mail
-function validateEmail() {
-    const regex = /^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,}$/;
-    if (!regex.test(emailInput.value)) {
-        emailInput.classList.add('invalid');
-        return false;
-    } else {
-        emailInput.classList.remove('invalid');
-        return true;
-    }
-}
-
-// Обработчик отправки формы
-form.addEventListener('submit', (event) => {
-    event.preventDefault(); // Отменяем стандартное поведение формы
-
-    // Проверяем все поля на валидность
-    const isNameValid = validateName();
-    const isPhoneValid = validatePhone();
-    const isEmailValid = validateEmail();
-
-    // Если все поля валидны, отправляем форму
-    if (isNameValid && isPhoneValid && isEmailValid) {
-        const formData = new FormData(form);
-        fetch(form.action, {
-            method: form.method,
-            body: formData
-        }).then(response => {
-            alert('Форма отправлена');
-            form.reset();
-        }).catch(error => {
-            alert('Произошла ошибка при отправке формы.');
-        });
-    } else {
-        alert('Пожалуйста, заполните форму корректно.');
-    }
-});
-
-// Обработчики изменения полей для удаления класса invalid
-nameInput.addEventListener('input', () => {
-    nameInput.classList.remove('invalid');
-});
-
-phoneInput.addEventListener('input', () => {
-    phoneInput.classList.remove('invalid');
-});
-
-emailInput.addEventListener('input', () => {
-    emailInput.classList.remove('invalid');
-});
